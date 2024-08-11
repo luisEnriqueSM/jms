3da608f1a22716eee044f7d6177fd1f8463b2b20
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-11 11:36:26 -0600

    Add model class

---
+++ src/main/java/org/beer/works/jms/model/HelloWorldMessage.java
@@ -0,0 +1,22 @@
+package org.beer.works.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -5996800369351524527L;
+
+    private UUID id;
+    private String message;
+
+}
